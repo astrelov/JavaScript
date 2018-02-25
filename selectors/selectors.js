e884969e8d0286181f13ec231bdff442d412241c
astrielov <krendelllllll@gmail.com>
2018-02-25 22:33:39 +0200

    things have changed

--- selectors/selectors.js
+++ selectors/selectors.js
@@ -16,14 +16,15 @@ function hasNextSibling(item, right) {
 function fetchElement(siblings, left, right, depth = 0) {
   dataArray.forEach((item) => {
     if (item.left === left) {
-      siblings.push(item);
-
-      const currItem = siblings[siblings.length - 1];
-      currItem.depth = depth;
+      const currItem = item;
+      let childs;
 
       if (hasChild(item)) {
-        currItem.childs = fetchElement([], item.left + 1, item.right - 1, depth + 1);
+        childs = fetchElement(new Map(), item.left + 1, item.right - 1, depth + 1);
       }
+
+      currItem.depth = depth;
+      siblings.set(currItem, childs);
 
       if (hasNextSibling(item, right)) {
         siblings = fetchElement(siblings, item.right + 1, right, depth);
@@ -36,13 +37,13 @@ function fetchElement(siblings, left, right, depth = 0) {
 function makeDocumentFragment(nestedSetData) {
   const fragment = document.createDocumentFragment();
 
-  function paste(array) {
-    array.forEach((x) => {
-      const op = new Option(x.name, x.id);
-      setLeftPadding.call(op, x.depth * 2);
+  function paste(nestedSetMap) {
+    nestedSetMap.forEach((value, key) => {
+      const op = new Option(key.name, key.id);
+      setLeftPadding.call(op, key.depth * 2);
       fragment.appendChild(op);
-      if (x.childs) {
-        paste(x.childs);
+      if (value) {
+        paste(value);
       }
     });
   }
@@ -56,7 +57,7 @@ fetch('https://sectors-enpoint.herokuapp.com/sectors')
   .then((array) => {
     dataArray = array.sort((a, b) => a.left - b.left);
 
-    const nestedSetData = fetchElement([], 1, dataArray.length * 2);
-    const fragment = makeDocumentFragment(nestedSetData);
+    const nestedSetDataMap = fetchElement(new Map(), 1, dataArray.length * 2);
+    const fragment = makeDocumentFragment(nestedSetDataMap);
     select.appendChild(fragment);
   });
